8be4c0350b88225be6130f743bcc5a723ee8c506
julianfie <julianfie64@gmail.com>
2026-07-15 13:17:59 +0200

    Añadido anotaciones en el metodo UserMapper

--- src/main/java/com/example/service/UserMapper.java
+++ src/main/java/com/example/service/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.dto.UserResponse;
 import com.example.entity.User;
@@ -8,6 +9,8 @@ import com.example.entity.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "name", source = "username")
+    @Mapping(target = "dob", source = "dateOfBirth")
     UserResponse mapUserToUserResponse(User user);
 
 }
